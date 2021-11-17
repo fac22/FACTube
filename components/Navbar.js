1512af6c6b240595a4200802c1bbe7fd119c4a5b
bubbles linux <appenzeller.barbara@gmail.com>
2021-11-17 18:40:21 +0000

    MUI Navbar icons: menu, search
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,31 +1,113 @@
 import React from 'react';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { styled, alpha } from '@mui/material/styles';
 import theme from './theme';
-// import SearchIcon from '@mui/icons-material/Search';
-// import {SearchIcon} from '@material-ui/icons';
-//import MenuIcon from '@material-ui/icons/Menu';
+import SearchIcon from '@mui/icons-material/Search';
+import MenuIcon from '@mui/icons-material/Menu';
+// import { SearchIcon, MenuIcon } from '@mui/icons-material';
 
 import {
   AppBar,
   Toolbar,
   Typography,
   IconButton,
-  MenuIcon,
+  Box,
+  InputBase,
+  TextField,
 } from '@material-ui/core';
+
+const Search = styled('div')(({ theme }) => ({
+  position: 'relative',
+  borderRadius: theme.shape.borderRadius,
+  // backgroundColor: alpha(theme.palette.common.white, 0.15),
+  // '&:hover': {
+  //   backgroundColor: alpha(theme.palette.common.white, 0.25),
+  // },
+  marginLeft: 0,
+  width: '100%',
+  [theme.breakpoints.up('sm')]: {
+    marginLeft: theme.spacing(1),
+    width: 'auto',
+  },
+}));
+
+const SearchIconWrapper = styled(IconButton)(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: '100%',
+  position: 'absolute',
+  pointerEvents: 'none',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: 'inherit',
+  '& .MuiInputBase-input': {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: '12ch',
+      '&:focus': {
+        width: '20ch',
+      },
+    },
+  },
+}));
 
 function Navbar() {
   return (
     <ThemeProvider theme={theme}>
-      <AppBar position="static" backgroundColor="primary">
-        <Toolbar>
-          <IconButton>
-            <MenuIcon></MenuIcon>
-          </IconButton>
-          <Typography variant="h1" color="secondary">
-            FAC-tube
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static" backgroundcolor="primary">
+          <Toolbar justify-content="space-between">
+            <IconButton aria-label="menu" color="inherit" sx={{ mr: 2 }}>
+              <MenuIcon />
+            </IconButton>
+            <Typography
+              variant="h1"
+              color="fontLight"
+              component="div"
+              // noWrap
+              sx={{ display: { xs: 'none', sm: 'block' } }}
+            >
+              FAC-tube
+            </Typography>
+            {/* <Search>
+              <InputBase
+                backgroundColor="secondary"
+                placeholder="Search..."
+                color="inherit"
+              />
+              <IconButton aria-label="search" color="inherit">
+                <SearchIcon />
+              </IconButton>
+            </Search> */}
+
+            <Search>
+              {/* <StyledInputBase */}
+              <TextField
+                // hiddenLabel
+                // defaultVlue="Small"
+                label="Search"
+                variant="filled"
+                type="search"
+                color="inherit"
+                // placeholder="Search…"
+                inputProps={{ 'aria-label': 'search' }}
+                // value={search}
+                // onChange={handleChange}
+              />
+              <SearchIconWrapper>
+                <SearchIcon />
+              </SearchIconWrapper>
+            </Search>
+          </Toolbar>
+        </AppBar>
+      </Box>
     </ThemeProvider>
   );
 }
