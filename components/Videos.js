d5606a0bbbff8a8b5ec748e6bb252a12ffb07d0b
“Moggach” <acunnane13@gmail.com>
2021-11-17 16:28:19 +0000

    adding first part of navbar
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>

--- components/Videos.js
+++ components/Videos.js
@@ -1,10 +1,9 @@
 import React from 'react';
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const YOUTUBE_API_KEY = 'AIzaSyDgoaw4-GM4V6YDPEAShzPSE7t0pWzBaGE';
 
 function Videos({ searchTerm }) {
-  console.log('search term', searchTerm);
   let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchTerm}&key=${YOUTUBE_API_KEY}`;
-
+  console.log('url', url);
   const [videoList, setVideoList] = React.useState([]);
   const [searchValue, setSearchValue] = React.useState();
 
@@ -17,25 +16,23 @@ function Videos({ searchTerm }) {
           return videoList;
         });
     }
-  }, [videoList, url, searchTerm]);
-
-  console.log('videoList', videoList);
+  }, [searchTerm]);
   let listArr = videoList.items;
-  console.log(listArr);
+  console.log('listArr', listArr);
   return (
     <div>
-      {/* <div className="video-list">
-        {videoList.map((video) => (
-          <div className="video-card" key={video.id.videoId}>
-            <p>{video.snippet.title}</p>
-            <h3>{video.snippet.channelTitle}</h3>
-            <p>{video.snippet.description}</p>
-            <iframe
-              src={`https://www.youtube.com/embed/${video.id.videoId}`}
-            ></iframe>
-          </div>
-        ))}
-      </div> */}
+      {/* //   <div className="video-list">
+    //     {videoList.map((video) => (
+    //       <div className="video-card" key={video.id.videoId}>
+    //         <p>{video.snippet.title}</p>
+    //         <h3>{video.snippet.channelTitle}</h3>
+    //         <p>{video.snippet.description}</p>
+    //         <iframe
+    //           src={`https://www.youtube.com/embed/${video.id.videoId}`}
+    //         ></iframe>
+    //       </div>
+    //     ))}
+    //   </div> */}
     </div>
   );
 }
