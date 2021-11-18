8a3dad3a94ca5df1aa5603593cb17c7ad0fa62b9
“Moggach” <acunnane13@gmail.com>
2021-11-18 17:02:14 +0000

    restructuring components, removing unecessary container
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- components/Layout.js
+++ components/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Navbar from './Navbar';
+import Box from '@material-ui/core/Box';
 
 function Layout({ children }) {
   return (
     <>
       <Navbar />
-      <main>{children}</main>
+      <Box sx={{ height: '100vh', fontFamily: 'Montserrat' }}>{children}</Box>
     </>
   );
 }
