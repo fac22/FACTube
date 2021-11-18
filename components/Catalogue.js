ce7c2a28b6449244953edb24f7f2343e3616c1fe
bubbles linux <appenzeller.barbara@gmail.com>
2021-11-18 13:55:31 +0000

    Can fetch our catalogue to Home
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: Barbara <77367593+0bubbles0@users.noreply.github.com>
    Co-authored-by: Danilo <81513148+danilo-cupido@users.noreply.github.com>

--- components/Catalogue.js
+++ components/Catalogue.js
@@ -1,30 +1,36 @@
 import React from 'react';
 import Videos from './Videos';
-import { savedVideos } from '../lib/database';
+import { ourVideos } from '../lib/database';
 
 function Catalogue() {
-  const ourVideoIds = savedVideos.map((video) => video.yt_id);
-  // console.log(ourVideoIds);
-  const [catalogue, setCatalogue] = React.useState(ourVideoIds);
+  // const [catalogue, setCatalogue] = React.useState();
 
-  const id = `&id=qz0aGYrrlhU&id=hQAHSlTtcmY&id=PkZNo7MFNFg`;
-  const ourVideoIdsUrl = savedVideos
-    .map((video) => `&id=${video.yt_id}`)
-    .join('');
-  console.log(ourVideoIdsUrl);
   const apiKey = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY;
   const details = `snippet%2CcontentDetails%2Cstatistics`;
-  let url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&id=qz0aGYrrlhU&id=hQAHSlTtcmY&id=PkZNo7MFNFg&key=${apiKey}`;
+  const ourVideoIds = ourVideos.map((video) => `&id=${video.yt_id}`).join('');
+
+  let url = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet${ourVideoIds}&key=${apiKey}`;
+
+  const loadCatalogue = () => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setCatalogue(data);
+      });
+  };
+  // loadCatalogue();  Bad!!! Maxxes out API key
 
   return (
     <>
       <h4>This is our catalogue</h4>
-      {ourVideoIds.map((yt_id) => (
+      {/* <Videos data={catalogue} /> */}
+      {/* {ourVideoIds.map((yt_id) => (
         <div>
           <p>{yt_id}</p>
           <iframe src={`https://www.youtube.com/embed/${yt_id}`}></iframe>
         </div>
-      ))}
+      ))} */}
     </>
   );
 }
