08977eae7da06bb570cb00a4018837e3236c9350
“Moggach” <acunnane13@gmail.com>
2021-11-22 14:12:04 +0000

    adding dummy video data to database file and output in video component

--- components/Videos.js
+++ components/Videos.js
@@ -1,10 +1,11 @@
 import ReactPlayer from 'react-player/youtube';
 import LikeButton from './LikeButton';
+import { dummyData } from '../lib/database';
 
-const Videos = ({ data }) => {
+const Videos = () => {
   return (
     <div className="video-list">
-      {data.map((video) => (
+      {dummyData.map((video) => (
         <div className="video-card" key={video.id.videoId}>
           <p>{video.snippet.title}</p>
           <h3>{video.snippet.channelTitle}</h3>
