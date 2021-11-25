43755bd18562625ce32ca1bfd8bf203bcb98fd98
“Moggach” <acunnane13@gmail.com>
2021-11-25 13:18:15 +0000

    adding video library where users can see added videos

--- pages/myvideos.js
+++ pages/myvideos.js
@@ -1,10 +1,26 @@
-const MyVideos = ({}) => {
+import MyVideos from '../components/MyVideos.js';
+import { useState, useEffect } from 'react';
+import { supabase } from '../lib/initSupabase';
+import Auth from '../components/Auth';
+
+export default function myVideosPage() {
+  const [session, setSession] = useState(null);
+
+  useEffect(() => {
+    setSession(supabase.auth.session());
+    console.log(supabase.auth.session());
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+  }, []);
+
   return (
     <>
-      <title>FAC-Tube: My ❤️ Videos</title>
-      <div>My ❤️ Videos</div>
+      {!session ? (
+        <Auth />
+      ) : (
+        <MyVideos key={session.user.id} session={session} />
+      )}
     </>
   );
-};
-
-export default MyVideos;
+}
